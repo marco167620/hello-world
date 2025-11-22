ab3b62148cda80859fc6b94aa7e7981584484ff8
Marco Lee (李正冬) <167620@o365.tku.edu.tw>
2025-11-22 14:48:00 +0800

    Create page.tsx 請增 about 頁面

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>關於 Marco</h1>
+      <p>這是我在淡江大學教學用的 Web3 測試站。</p>
+      <p>目前運行在 Vercel Edge Network 上。</p>
+    </div>
+  );
+}
